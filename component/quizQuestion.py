786318e092d52458597cfc0d9c5b56e7befdb2cd
Your Name <you@example.com>
2021-12-07 22:44:11 -0500

    updating stracture

---
+++ component/quizQuestion.py
@@ -0,0 +1,31 @@
+questions = {
+    "q1" : {
+        "question": "Do you like tea?",
+        "yes" : 10,
+        "no" : 0,
+    },
+
+    "q2" : {
+        "question" : "DO you do sport?",
+        "yes" : 10,
+        "no" : 0,
+    },
+
+    "q3" : {
+        "question" : "Are you student?",
+        "yes" : 10,
+        "no" : 0,
+    },
+
+    "q4" : {
+        "question" : "Do you study IDP?",
+        "yes" : 10,
+        "no" : 0,
+    },
+
+    "q5" : {
+        "question" : "Do you like colors?",
+        "yes" : 10,
+        "no" : 0,
+    }
+}
