2abbf7c35ba5a1ed56a4d7029145dd5a224c0c7a
Your Name <you@example.com>
2021-12-08 01:49:24 -0500

    adding color

--- component/vars.py
+++ component/vars.py
@@ -4,4 +4,4 @@ from PIL.Image import Image
 quizTotal = 0
 
 charcter = ["Tulip", "Daffodil", "Sunflower", "Bluebell", "Cherry blossom"]
-Image = ["Tulip.jpg", "Daffodil.png", "Sunflower.jpg", "Bluebell.jpg", "Cherry blossom.jpg"]
+Images = ["Tulip.jpg", "Daffodil.png", "Sunflower.jpg", "Bluebell.jpg", "Cherry blossom.jpg"]
